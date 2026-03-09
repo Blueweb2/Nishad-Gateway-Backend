9ab7c98e00005855dd1e2da6ce7646dd546adf4a
Sabithabasima <sabithabasimavk@gmail.com>
2026-03-09 18:23:08 +0530

    log err in leads fixing

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 
 export const mailer = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // important: false for 587
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
